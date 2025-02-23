7b18d21f6f24b7feb19238068f976a58232d36da
Adrian Jasaroska <adrianjasaroska@gmail.com>
2025-02-23 19:19:01 +0100

    I have changed the styling, im not sure if ill make it to the deadline

--- App/Client/Scripts/index.js
+++ App/Client/Scripts/index.js
@@ -3,21 +3,23 @@ const messageForm = document.getElementById('send-container')
 const messageContainer = document.getElementById('message-container')
 const messageInput = document.getElementById('message-input')
 
-socket.on('chat-message', data => {
-    appendMessage(data)
+socket.on('chat-message', message => {
+    appendMessage(message, 'received')
 })
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
+    appendMessage(`You: ${message}`, 'sent')
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
 
-function appendMessage(message) {
+function appendMessage(message, sender) {
     console.log("Appending Message: ", message)
     const messageElement = document.createElement('div')
     messageElement.innerText = message
+    messageElement.classList.add(sender === 'sent' ? 'sent-message' : 'received-message')
     messageContainer.append(messageElement)
     console.log(messageContainer)
 }
